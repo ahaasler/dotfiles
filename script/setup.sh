bd2647b776acff389d85b928205e60a1f5d49c95
Adrian Haasler García <dev@adrianhaasler.com>
2016-08-14 15:45:46 +0000

    Exit setup script if dotfiles folder doesn't exist
    Also modify fail function to exit with a non zero status code.

--- script/setup.sh
+++ script/setup.sh
@@ -25,11 +25,13 @@ success () {
 }
 
 # Author: Zach Holman <zach@zachholman.com>
-# License: MIT (licenses/holman-dotfiles.md)
+# Modifications: Adrian Haasler <dev@adrianhaasler.com>
+# - Exit with non zero status code
+# License: MIT (licenses/holman-dotfiles.md) and MIT (LICENSE) for modifications
 fail () {
 	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
 	echo ''
-	exit
+	exit 1
 }
 
 # Author: Zach Holman <zach@zachholman.com>
@@ -124,5 +126,9 @@ install_dotfiles () {
 		link_file "$src" "$dst"
 	done
 }
+
+if [ ! -d "$DOTFILES_HOME" ]; then
+	fail "$DOTFILES_HOME doesn't exist"
+fi
 
 install_dotfiles
