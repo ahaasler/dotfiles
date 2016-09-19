fe14f9c9908a91a07c0b0024b31d498468b1cbb2
Adrian Haasler García <dev@adrianhaasler.com>
2016-09-19 17:48:31 +0000

    Beautify get script output
    The code is duplicated because this script is supposed to be executed on
    its own.
    
    [ci skip]

--- script/get.sh
+++ script/get.sh
@@ -3,18 +3,46 @@
 set -e
 set -o pipefail
 
+# Author: Zach Holman <zach@zachholman.com>
+# License: MIT (licenses/holman-dotfiles.md)
+info () {
+	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+# Author: Zach Holman <zach@zachholman.com>
+# License: MIT (licenses/holman-dotfiles.md)
+user () {
+	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+# Author: Zach Holman <zach@zachholman.com>
+# License: MIT (licenses/holman-dotfiles.md)
+success () {
+	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+# Author: Zach Holman <zach@zachholman.com>
+# Modifications: Adrian Haasler <dev@adrianhaasler.com>
+# - Exit with non zero status code
+# License: MIT (licenses/holman-dotfiles.md) and MIT (LICENSE) for modifications
+fail () {
+	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+	echo ''
+	exit 1
+}
+
 if [ -z "$DOTFILES_HOME" ]; then
 	DOTFILES_HOME="$HOME/.dotfiles"
 fi
 
 mkdir -p $DOTFILES_HOME
 
-echo "installing dotfiles in $DOTFILES_HOME"
+info "installing dotfiles in $DOTFILES_HOME"
 
-hash curl 2>/dev/null || { echo >&2 "curl is not installed, please install it"; exit 1; }
+hash curl 2>/dev/null || { fail >&2 "curl is not installed, please install it"; }
 
 releaseUrl=$(curl -s https://api.github.com/repos/ahaasler/dotfiles/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
-echo "downloading latest release: $releaseUrl"
+info "downloading latest release: $releaseUrl"
 curl -skL $releaseUrl | tar zx --strip-components 1 -C $DOTFILES_HOME
 
 $DOTFILES_HOME/script/setup.sh
