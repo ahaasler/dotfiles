74e38688d5cb6587356bce2fcfa71d4bdfe905ef
Adrian Haasler García <dev@adrianhaasler.com>
2016-12-23 06:13:20 +0000

    Add human dates in ll and add dus for size ord ls

--- shell/alias.sh
+++ shell/alias.sh
@@ -1,9 +1,10 @@
 alias fucking=sudo
 alias bd='cd $OLDPWD'
 alias ls='ls --color=auto'
-alias ll='ls -l'
+alias ll='ls -lh'
 alias lla='ll -a'
 alias grep='grep --color=auto'
 alias egrep='grep -E'
 alias fgrep='grep -F'
 alias man='LC_ALL=C LANG=C man'
+alias dus='du -hsc * | sort -h'
