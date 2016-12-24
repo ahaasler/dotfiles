5337414a736dd4de58390255d5d27198685f3031
Adrian Haasler García <dev@adrianhaasler.com>
2016-12-24 20:03:42 +0000

    Avoid code dup in get script
    [ci skip]

--- script/get.sh
+++ script/get.sh
@@ -10,38 +10,7 @@ args="$@"
 debug=false
 
 # Functions
-
-debug () {
-	if $debug; then printf "\r  [\033[0;35mDEBG\033[0m] $1\n"; fi
-}
-
-# Author: Zach Holman <zach@zachholman.com>
-# License: MIT (licenses/holman-dotfiles.md)
-info () {
-	printf "\r  [ \033[00;34m..\033[0m ] $1\n"
-}
-
-# Author: Zach Holman <zach@zachholman.com>
-# License: MIT (licenses/holman-dotfiles.md)
-user () {
-	printf "\r  [ \033[0;33m??\033[0m ] $1\n"
-}
-
-# Author: Zach Holman <zach@zachholman.com>
-# License: MIT (licenses/holman-dotfiles.md)
-success () {
-	printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
-}
-
-# Author: Zach Holman <zach@zachholman.com>
-# Modifications: Adrian Haasler <dev@adrianhaasler.com>
-# - Exit with non zero status code
-# License: MIT (licenses/holman-dotfiles.md) and MIT (LICENSE) for modifications
-fail () {
-	printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
-	echo ''
-	exit 1
-}
+source <(curl -skL https://raw.githubusercontent.com/ahaasler/dotfiles/master/script/function/log.sh)
 
 home="$HOME/.dotfiles"
 
