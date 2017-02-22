ea70a63eb683cb60905355ebbcba02335a4c4d82
Adrian Haasler García <dev@adrianhaasler.com>
2017-02-22 21:44:13 +0000

    Use ascii powerline theme in login consoles

--- shell/export.sh
+++ shell/export.sh
@@ -3,3 +3,5 @@ if [ -d "$HOME/bin" ]; then
 fi
 export PATH=$PATH:$DOTFILES_HOME/shell/powerline/scripts
 export TERM=screen-256color
+# On login use ascii
+[ "$(ps -o comm= $PPID)" == "login" ] || [ "$(tmux display-message -p '#S')" == "login" ] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
