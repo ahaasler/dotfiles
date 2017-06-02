34a5c194f64cfecd8091a36b22b72531fde51fa5
Adrian Haasler García <dev@adrianhaasler.com>
2017-06-02 15:16:11 +0000

    Use aliases on git functions

--- git/function.sh
+++ git/function.sh
@@ -1,9 +1,9 @@
 gclcd() {
-	git clone "$1" && cd "$(basename "$1" | sed 's/\.git//g')"
+	gcl "$1" && cd "$(basename "$1" | sed 's/\.git//g')"
 }
 gd^() {
-	git diff $1^ $1
+	gd $1^ $1
 }
 gdt^() {
-	git difftool $1^ $1
+	gdt $1^ $1
 }
