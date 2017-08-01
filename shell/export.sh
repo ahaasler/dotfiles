97eeb5886fb88b6f985e021c6eaa6c5f04495ec7
Adrian Haasler García <dev@adrianhaasler.com>
2017-08-01 20:26:03 +0200

    Use double brackets for conditions

--- shell/export.sh
+++ shell/export.sh
@@ -5,4 +5,4 @@ export PATH=$HOME/.local/bin:$PATH
 export PATH=$PATH:$DOTFILES_HOME/shell/powerline/scripts
 export TERM=screen-256color
 # On login use ascii
-[ "$(ps -o comm= $PPID)" == "login" ] || [ "$(tmux display-message -p '#S')" == "login" ] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
+[[ "$(ps -o comm= $PPID)" == "login" ]] || [[ "$(tmux display-message -p '#S')" == "login" ]] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
