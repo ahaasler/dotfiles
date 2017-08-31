8e9bbbdba17e60366af7eea1d7719df62f71cd02
Adrian Haasler García <dev@adrianhaasler.com>
2017-08-31 20:27:53 +0000

    Increase search depth to source dotfiles

--- script/function/source.sh
+++ script/function/source.sh
@@ -4,7 +4,7 @@ source_dotfiles_pattern() {
 	local folder="$DOTFILES_HOME/$2"
 	local pattern="$1"
 	local blacklist="$3"
-	for file in $(find -H "$folder" -maxdepth 2 -name "$pattern" -not -path '*/.git/**' -not -path "$folder/if/**" -not -path "$blacklist")
+	for file in $(find -H "$folder" -maxdepth 3 -name "$pattern" -not -path '*/.git/**' -not -path "$folder/if/**" -not -path "$blacklist")
 	do
 		source $file
 	done
