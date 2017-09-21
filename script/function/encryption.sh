17f0af3340c8ef07f8d8e7b3443f7d1f5666cb55
Adrian Haasler García <dev@adrianhaasler.com>
2017-09-21 21:04:20 +0000

    Add optional target folder in encrypt function

--- script/function/encryption.sh
+++ script/function/encryption.sh
@@ -4,9 +4,14 @@
 
 dotfiles_encrypt() {
 	local file="$1"
+	local target="$2/$(basename "$1")"
+	if [ -z "$2" ]; then
+		target="$1"
+	fi
+	mkdir -p $(dirname $target)
 	local pass="$(openssl rand -base64 170)"
-	openssl aes-256-cbc -in $file -out $file.enc -pass pass:"$pass"
-	echo "$pass" | openssl rsautl -encrypt -pubin -inkey <(ssh-keygen -e -f ~/.ssh/id_rsa.pub -m PKCS8) -out $file.key
+	openssl aes-256-cbc -in $file -out $target.enc -pass pass:"$pass"
+	echo "$pass" | openssl rsautl -encrypt -pubin -inkey <(ssh-keygen -e -f ~/.ssh/id_rsa.pub -m PKCS8) -out $target.key
 }
 
 dotfiles_decrypt() {
