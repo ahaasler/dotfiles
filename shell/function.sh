c4bf870ed2c4ba93dce06a26fc454366f54f3929
Adrian Haasler García <dev@adrianhaasler.com>
2017-10-04 21:09:58 +0000

    Add function to search in multiple paths

--- shell/function.sh
+++ shell/function.sh
@@ -1,3 +1,15 @@
 mkcd() {
 	mkdir -p -- "$1" && cd -- "$1"
 }
+
+f() {
+	file="$1"
+	shift
+	if [ "$#" = "0" ]; then
+		set "."
+	fi
+	for d in "$@"; do
+		find "$d" -iname "*$file*"
+		shift
+	done
+}
