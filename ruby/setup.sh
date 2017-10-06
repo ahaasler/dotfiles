da55c6ec604866580fb11d736bce9b4cec5871c4
Adrian Haasler García <dev@adrianhaasler.com>
2017-10-06 18:08:44 +0000

    Add ruby gems installation step

---
+++ ruby/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check gem installed
+hash gem 2>/dev/null || return
+
+info 'installing common ruby gems'
+if ( cat "$DOTFILES_HOME/ruby/software.txt" | xargs gem install --user-install ) 2>&1 | while read line
+	do
+		debug "$line";
+	done
+then
+	success "installed ruby gems"
+else
+	warn "ruby gems installation failed"
+fi
