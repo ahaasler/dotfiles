7affe4e16a0c9f04cf02e500dc93209250863000
Adrian Haasler García <dev@adrianhaasler.com>
2017-10-19 18:45:48 +0000

    Add node modules installation step

---
+++ node/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check npm installed
+hash npm 2>/dev/null || return
+
+info 'installing common node modules'
+if ( cat "$DOTFILES_HOME/node/software.txt" | xargs npm install -g ) 2>&1 | while read line
+	do
+		debug "$line";
+	done
+then
+	success "installed node modules"
+else
+	warn "node modules installation failed"
+fi
