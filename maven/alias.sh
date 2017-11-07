fceb63b1fe2748969269f960dc22ad693039765d
Adrian Haasler García <dev@adrianhaasler.com>
2017-11-07 17:59:32 +0000

    Add alias to display maven current project version

--- maven/alias.sh
+++ maven/alias.sh
@@ -1,3 +1,4 @@
 alias mcp='mvn clean package'
 alias mci='mvn clean install'
 alias mcin='mci -N'
+alias mpv='mvn -q -Dexec.executable="echo" -Dexec.args="\${project.version}" --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec'
