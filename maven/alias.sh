c6100380b0e1e9afc968bceb4e42b1ad7fdd4c81
Adrian Haasler García <dev@adrianhaasler.com>
2017-11-09 22:05:18 +0000

    Add mpv faster alternative if xmllint is available

--- maven/alias.sh
+++ maven/alias.sh
@@ -2,3 +2,4 @@ alias mcp='mvn clean package'
 alias mci='mvn clean install'
 alias mcin='mci -N'
 alias mpv='mvn -q -Dexec.executable="echo" -Dexec.args="\${project.version}" --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec'
+hash xmllint 2>/dev/null && alias mpv="xmllint --xpath '//*[local-name()=\"project\"]/*[local-name()=\"version\"]/text()' pom.xml | xargs echo"
