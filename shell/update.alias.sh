e93d3de0c35762cb70308ba91cfefc43d622a25e
Adrian Haasler García <dev@adrianhaasler.com>
2017-11-28 22:08:13 +0000

    Source dotfiles on udd alias

--- shell/update.alias.sh
+++ shell/update.alias.sh
@@ -1,4 +1,4 @@
 alias update-dotfiles="$DOTFILES_HOME/script/update.sh"
 hash ud 2>/dev/null || alias ud=update-dotfiles
-alias udd='update-dotfiles -d'
+alias udd='update-dotfiles -d; srcd'
 alias srcd='source ~/.profile'
