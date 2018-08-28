30ecd7caafdfafc6e4f53d7393821f69ec280981
Adrian Haasler García <dev@adrianhaasler.com>
2018-08-28 08:29:19 +0000

    Add he script to PATH

--- shell/export.sh
+++ shell/export.sh
@@ -3,6 +3,7 @@ if [ -d "$HOME/bin" ]; then
 fi
 export PATH=$HOME/.local/bin:$PATH
 export PATH=$PATH:$DOTFILES_HOME/shell/powerline/scripts
+export PATH=$PATH:$DOTFILES_HOME/shell/bin
 export TERM=screen-256color
 # On login use ascii
 [[ "$(ps -o comm= $PPID)" == "login" ]] || [[ "$(tmux display-message -p '#S')" == "login" ]] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
