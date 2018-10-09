b6f9bb2d404d70ff76248dc523d8a53de74a253e
Adrian Haasler García <dev@adrianhaasler.com>
2018-10-09 06:24:19 +0000

    Update sdkman to 5.7.3+337

--- sdk/export.sh
+++ sdk/export.sh
@@ -1,5 +1,5 @@
 # Initialize SDKMAN variables, init script has not been processed in git.
-export SDKMAN_VERSION="5.6.1+290"
+export SDKMAN_VERSION="5.7.3+337"
 export SDKMAN_LEGACY_API="https://api.sdkman.io/1"
 export SDKMAN_CURRENT_API="https://api.sdkman.io/2"
 export SDKMAN_DIR="$HOME/.sdkman"
