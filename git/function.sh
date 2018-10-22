d263fa81eb40df978a58a510db49f07dc20b0eb8
Adrian Haasler García <adrian.haasler@amplia.es>
2018-10-22 17:01:31 +0200

    Pass arguments in gd^ and gdt^ functions

--- git/function.sh
+++ git/function.sh
@@ -2,10 +2,10 @@ gclcd() {
 	gcl "$1" && cd "$(basename "$1" | sed 's/\.git//g')"
 }
 gd^() {
-	gd $1^ $1
+	gd $1^ $1 ${@:2}
 }
 gdt^() {
-	gdt $1^ $1
+	gdt $1^ $1 ${@:2}
 }
 gtam() {
 	gta "$1" -m "$2"
--- shell/colorize.alias.sh
+++ shell/colorize.alias.sh
@@ -5,3 +5,8 @@ alias blue='colorize -b'
 alias purple='colorize -p'
 alias cyan='colorize -c'
 alias white='colorize -w'
+
+# Author: Dennis Kaarsemaker (https://unix.stackexchange.com/a/55547)
+# License: cc by-sa 3.0 (licenses/cc-by-sa-3.0.txt)
+alias uncolorize='sed -r "s/\x1B\[[0-9;]*[JKmsu]//g"'
+alias nocolor='uncolorize'
