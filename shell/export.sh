942871e61fc1d008c02d7b7818e354d23161264d
Adrian Haasler García <dev@adrianhaasler.com>
2019-09-22 14:50:05 +0200

    Move all user binary files to ~/.local/bin

--- shell/export.sh
+++ shell/export.sh
@@ -1,9 +1,4 @@
-if [ -d "$HOME/bin" ]; then
-	export PATH=$HOME/bin:$PATH
-fi
 export PATH=$HOME/.local/bin:$PATH
-export PATH=$PATH:$DOTFILES_HOME/shell/powerline/scripts
-export PATH=$PATH:$DOTFILES_HOME/shell/bin
 export TERM=screen-256color
 # On login use ascii
 [[ "$(ps -o comm= $PPID)" == "login" ]] || [[ "$(tmux display-message -p '#S')" == "login" ]] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
