42baede7a70f45b97975dffd3c9e5c9b809a0031
Adrian Haasler García <dev@adrianhaasler.com>
2019-09-29 19:27:29 +0200

    Display sway! as ASCII art while loading

--- sway/zzz-sway.sh
+++ sway/zzz-sway.sh
@@ -1,6 +1,12 @@
 if [[ "$(tty)" == '/dev/tty1' ]]; then
 	hash sway 2>/dev/null || return
-	echo "Starting Sway!"
+	printf '\nStarting Sway!\n\n'
+	printf '   \e[1;39m                        \e[0;36m _ \e[0m\n'
+	printf '   \e[1;39m _____      ____ _ _   _\e[0;36m| |\e[0m\n'
+	printf '   \e[1;39m/ __\\ \\ /\\ / / _` | | | \e[0;36m| |\e[0m\n'
+	printf '   \e[1;39m\\__ \\\\ V  V / (_| | |_| \e[0;36m|_|\e[0m\n'
+	printf '   \e[1;39m|___/ \\_/\\_/ \\__,_|\\__, \e[0;36m(_)\e[0m\n'
+	printf '   \e[1;39m                   |___/   \e[0m\n\n'
 	case $SHELL in
 		*/bash) if [ -f $HOME/.profile ]; then . $HOME/.profile; fi ;;
 		*/zsh) [ -f $HOME/.zprofile ] && . $HOME/.zprofile ;;
