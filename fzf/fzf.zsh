028a4ad62ea728eaae4b8a8fc96a4c049aabbd5e
Adrian Haasler García <dev@adrianhaasler.com>
2019-10-01 23:57:17 +0200

    Add fzf completions and key bindings for zsh

---
+++ fzf/fzf.zsh
@@ -0,0 +1,9 @@
+# Completion
+if [[ -r /usr/share/fzf/completion.zsh ]]; then
+	source /usr/share/fzf/completion.zsh
+fi
+
+# Key bindings
+if [[ -r /usr/share/fzf/key-bindings.zsh ]]; then
+	source /usr/share/fzf/key-bindings.zsh
+fi
