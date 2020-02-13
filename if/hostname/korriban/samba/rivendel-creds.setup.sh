97adee68809de697a63ab51ba07c139bece204b4
Adrian Haasler García <adrian.haasler@amplia.es>
2020-02-13 17:21:10 +0100

    Add rivendel creds generator

---
+++ if/hostname/korriban/samba/rivendel-creds.setup.sh
@@ -0,0 +1,15 @@
+FILE="/etc/samba/creds/rivendel"
+TEMPLATE="$DOTFILES_HOME/if/hostname/korriban/samba/rivendel-creds.template"
+
+generate_rivendel_creds() {
+	local password=
+	info "setting up rivendel credentials"
+	userline "password for rivendel: "
+	read -s password
+	echo # reset prompt because userline
+	info "root necessary to setup rivendel credentials"
+	sed -e "s/\$password/$password/g" "$TEMPLATE" | sudo tee "$FILE" 1>/dev/null
+}
+
+[ -L "$FILE" ] && info "root necessary to clean up rivendel credentials" && sudo unlink "$FILE"
+[ -f "$FILE" ] || generate_rivendel_creds
