aad61e7e25aa69373a778c3d238c9811572df892
Adrian Haasler García <dev@adrianhaasler.com>
2020-02-20 23:35:31 +0100

    Remove virtualenvwrapper
    This reverts commit ae925c4d622ede662a09add485fb3cfaff5e6f79.

--- python/venv.export.sh
+++
@@ -1,7 +0,0 @@
-export WORKON_HOME="$HOME/.virtualenvs"
-export PIP_VIRTUALENV_BASE="$WORKON_HOME"
-export PIP_RESPECT_VIRTUALENV=true
-
-if [ -f $HOME/.local/bin/virtualenvwrapper.sh ]; then
-	source $HOME/.local/bin/virtualenvwrapper.sh
-fi
