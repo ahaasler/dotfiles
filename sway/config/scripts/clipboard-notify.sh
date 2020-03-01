c08ec485416574ca2ca9b0ddbaafa9edb55fd515
Adrian Haasler García <dev@adrianhaasler.com>
2020-03-01 16:02:55 +0100

    Sanitize clipboard notify last file contents

--- sway/config/scripts/clipboard-notify.sh
+++ sway/config/scripts/clipboard-notify.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 if hash notify-send.sh 2>/dev/null; then
-	notify-send.sh 'Copied to clipboard' "${1}" --icon=edit-paste -t 3000 -a 'sway' -r $(cat ~/.local/share/clipboard-notify-last.txt) -p > ~/.local/share/clipboard-notify-last.txt
+	previousFile=~/.local/share/clipboard-notify-last.txt
+	previousId="$(tr -d '\n' < $previousFile)"
+	if ! [[ "$previousId" =~ ^[0-9]+$ ]]; then
+		notify-send.sh 'Copied to clipboard' "${1}" --icon=edit-paste -t 3000 -a 'sway' -p > "$previousFile"
+	else
+		notify-send.sh 'Copied to clipboard' "${1}" --icon=edit-paste -t 3000 -a 'sway' -r $previousId -p > "$previousFile"
+	fi
 elif hash notify-send 2>/dev/null; then
 	notify-send 'Copied to clipboard' "${1}" --icon=edit-paste -t 2000 -a 'sway'
 fi
