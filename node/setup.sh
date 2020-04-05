fd07a3db8392254220e1cdbb62e745fbac9cb2e8
Adrian Haasler García <dev@adrianhaasler.com>
2020-04-05 18:50:17 +0200

    Avoid node setup error if npm is missing

--- node/setup.sh
+++ node/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check npm installed
-hash npm 2>/dev/null || return
+hash npm 2>/dev/null || return 0
 
 info 'installing common node modules'
 if ( cat "$DOTFILES_HOME/node/software.txt" | xargs npm install -g ) 2>&1 | while read line
