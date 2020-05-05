4e22d97294414ead67c2fc9db56273cb7df63fc9
Adrian Haasler García <dev@adrianhaasler.com>
2020-05-05 18:45:59 +0200

    Fix tmux not installed sourcing profile

--- shell/export.sh
+++ shell/export.sh
@@ -1,4 +1,4 @@
 export PATH=$HOME/.local/bin:$PATH
 export TERM=screen-256color
 # On login use ascii
-[[ "$(ps -o comm= $PPID)" == "login" ]] || [[ "$(tmux display-message -p '#S')" == "login" ]] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
+[[ "$(ps -o comm= $PPID)" == "login" ]] || [[ "$(hash tmux 2> /dev/null && tmux display-message -p '#S')" == "login" ]] && export POWERLINE_CONFIG_OVERRIDES="common.default_top_theme=ascii"
