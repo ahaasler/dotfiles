a1044507a2f2db4141b2871222c105149d621316
Adrian Haasler García <dev@adrianhaasler.com>
2020-10-15 19:16:46 +0200

    Customize fzf colors to match terminal

--- fzf/export.sh
+++ fzf/export.sh
@@ -1,2 +1,6 @@
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
- --prompt "❯ " --pointer "❯" --marker "❯"'
+ --prompt "❯ " --pointer "❯" --marker "❯"
+ --color "fg:7,bg:-1,hl:2"
+ --color "fg+:7,bg+:-1,hl+:2"
+ --color "info:3,prompt:6,pointer:1"
+ --color "marker:1,spinner:2,header:4"'
