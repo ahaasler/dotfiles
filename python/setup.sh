16a5a0a7a4f8bce615233bf639fbe23bc32ab4f3
Adrian Haasler García <adrian.haasler@amplia.es>
2021-04-13 19:33:50 +0200

    Fix python and ruby setups when missing

--- python/setup.sh
+++ python/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check pip installed
-hash pip 2>/dev/null || return
+hash pip 2>/dev/null || return 0
 
 info 'installing common python modules'
 if ( pip install --upgrade --user -r "$DOTFILES_HOME/python/software.txt" ) 2>&1 | while read line
--- ruby/setup.sh
+++ ruby/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check gem installed
-hash gem 2>/dev/null || return
+hash gem 2>/dev/null || return 0
 
 info 'installing common ruby gems'
 if ( cat "$DOTFILES_HOME/ruby/software.txt" | xargs gem install --user-install ) 2>&1 | while read line
