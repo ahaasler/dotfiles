ca6c71ee7cd9648d3d82da87551e10113b101062
Adrian Haasler García <dev@adrianhaasler.com>
2021-04-16 16:29:41 +0200

    Add spring-boot maven aliases

--- maven/alias.sh
+++ maven/alias.sh
@@ -2,6 +2,8 @@ alias mcp='mvn clean package'
 alias mci='mvn clean install'
 alias mcin='mci -N'
 alias mdt='mvn dependency:tree'
+alias msr='mvn spring-boot:run'
+alias msbi='mvn spring-boot:build-image'
 alias mvs='mvn versions:set -DgenerateBackupPoms=false'
 alias mpv='mvn -q -Dexec.executable="echo" -Dexec.args="\${project.version}" --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec'
 hash xmllint 2>/dev/null && alias mpv="xmllint --xpath '//*[local-name()=\"project\"]/*[local-name()=\"version\"]/text()' pom.xml | xargs echo"
