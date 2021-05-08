4231bc90ba4c7e9bb323489224a2c6c321823eaa
Adrian Haasler García <dev@adrianhaasler.com>
2021-05-08 18:02:21 +0200

    refactor(python)!: remove pyenv and pyenv-virtualenv
    this functionality will be provided by asdf with additional functions and aliases
    
    BREAKING CHANGE: pyenv and its aliases are no longer available
    
    Refs #30

--- python/pyenv.export.sh
+++
@@ -1,7 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-
-if hash pyenv 2>/dev/null; then
-	eval "$(pyenv init -)"
-fi
--- python/pyenv.function.sh
+++
@@ -1,3 +0,0 @@
-workon() {
-	if [ $# -eq 0 ]; then pyenv virtualenvs --skip-aliases --bare; else pyenv activate $1; fi
-}
