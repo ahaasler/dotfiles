9b5d0a08106df36eecc8bf9ede0589ec975d60e3
Adrian Haasler García <dev@adrianhaasler.com>
2021-06-02 20:13:05 +0200

    fix(sway): disable asdf version manager
    Refs: #30, fixes #32

--- sway/zzz-sway.sh
+++ sway/zzz-sway.sh
@@ -7,11 +7,13 @@ if [[ "$(tty)" == '/dev/tty1' ]]; then
 	printf '   \e[1;39m\\__ \\\\ V  V / (_| | |_| \e[0;36m|_|\e[0m\n'
 	printf '   \e[1;39m|___/ \\_/\\_/ \\__,_|\\__, \e[0;36m(_)\e[0m\n'
 	printf '   \e[1;39m                   |___/   \e[0m\n\n'
+	ASDF_DISABLE=true
 	case $SHELL in
 		*/bash) if [ -f $HOME/.profile ]; then . $HOME/.profile; fi ;;
 		*/zsh) [ -f $HOME/.zprofile ] && ZSH_FORCE_PROFILE_LOAD=true . $HOME/.zprofile ;;
 		*) [ -f $HOME/.profile ] && . $HOME/.profile ;;
 	esac
+	unset ASDF_DISABLE
 	export POWERLINE_CONFIG_OVERRIDES=""
 	export XDG_CURRENT_DESKTOP=sway
 	export MOZ_ENABLE_WAYLAND=1
