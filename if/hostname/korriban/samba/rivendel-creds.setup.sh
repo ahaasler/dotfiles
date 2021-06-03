a59bd90cdf64da41412ee40ca3503a964c58ace3
Adrian Haasler García <adrian.haasler@amplia.es>
2021-06-03 16:45:03 +0200

    fix(samba): escape passwords entered during mustafar & korriban setups

--- if/hostname/korriban/samba/rivendel-creds.setup.sh
+++ if/hostname/korriban/samba/rivendel-creds.setup.sh
@@ -7,8 +7,10 @@ generate_rivendel_creds() {
 	userline "password for rivendel: "
 	read -s password
 	echo # reset prompt because userline
+	local escaped_password
+	escaped_password=$(printf '%s\n' "$password" | sed -e 's/[\/&]/\\&/g')
 	info "root necessary to setup rivendel credentials"
-	sed -e "s/\$password/$password/g" "$TEMPLATE" | sudo tee "$FILE" 1>/dev/null
+	sed -e "s/\$password/${escaped_password}/g" "$TEMPLATE" | sudo tee "$FILE" 1>/dev/null
 }
 
 [ -L "$FILE" ] && info "root necessary to clean up rivendel credentials" && sudo unlink "$FILE"
