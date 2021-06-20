0b6953bb7330d0b4ae3477b2eac135c31027a747
Adrian Haasler García <dev@adrianhaasler.com>
2021-06-20 18:19:04 +0200

    fix(python): fix dotfiles python installs
    disable asdf python when installing common python modules
    
    Refs: #30

--- python/setup.sh
+++ python/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check pip installed
-hash pip 2>/dev/null || return 0
+ASDF_PYTHON_VERSION=system hash pip 2>/dev/null || return 0
 
 info 'installing common python modules'
-if ( pip install --upgrade --user -r "$DOTFILES_HOME/python/software.txt" ) 2>&1 | while read line
+if ( ASDF_PYTHON_VERSION=system pip install --upgrade --user -r "$DOTFILES_HOME/python/software.txt" ) 2>&1 | while read line
 	do
 		debug "$line";
 	done
