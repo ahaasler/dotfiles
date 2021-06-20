b93dbe952d194f6215e81a80a6c0d998b08dd6f7
Adrian Haasler García <dev@adrianhaasler.com>
2021-06-20 18:35:58 +0200

    fix(python): fix installing dotfiles modules on venv

--- python/setup.sh
+++ python/setup.sh
@@ -3,6 +3,12 @@
 # Check pip installed
 ASDF_PYTHON_VERSION=system hash pip 2>/dev/null || return 0
 
+# Check not in virtual environment
+if [ -n "$VIRTUAL_ENV" ]; then
+	warn "python modules installation skipped because of venv"
+	return 0
+fi
+
 info 'installing common python modules'
 if ( ASDF_PYTHON_VERSION=system pip install --upgrade --user -r "$DOTFILES_HOME/python/software.txt" ) 2>&1 | while read line
 	do
