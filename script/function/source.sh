ad6a02ce334476a88833cf991152b1ae86191ad9
Adrian Haasler García <dev@adrianhaasler.com>
2021-06-26 14:05:49 +0200

    refactor: change blacklist to blocklist

--- script/function/source.sh
+++ script/function/source.sh
@@ -4,8 +4,8 @@ source_dotfiles_pattern() {
 	hash find 2>/dev/null || return 1
 	local folder="$DOTFILES_HOME/$2"
 	local pattern="$1"
-	local blacklist="$3"
-	for file in $(find -H "$folder" -maxdepth 3 -name "$pattern" -not -path '*/.git/**' -not -path "$folder/if/**" -not -path "$blacklist")
+	local blocklist="$3"
+	for file in $(find -H "$folder" -maxdepth 3 -name "$pattern" -not -path '*/.git/**' -not -path "$folder/if/**" -not -path "$blocklist")
 	do
 		source $file
 	done
