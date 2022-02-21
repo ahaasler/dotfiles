d614e3c6ff1ef712b20f95b225a74e8a93df8d5d
Adrian Haasler García <dev@adrianhaasler.com>
2022-02-21 21:55:34 +0100

    fix(sway): try different menu options until one works

--- sway/config/scripts/app-menu.sh
+++ sway/config/scripts/app-menu.sh
@@ -1,15 +1,34 @@
-if hash ulauncher 2>/dev/null; then
-	ulauncher-toggle
-elif hash wofi 2>/dev/null; then
+#!/usr/bin/env bash
+RESULT=1
+if [ $RESULT -ne 0 ] && hash ulauncher 2>/dev/null; then
+	dbus-send \
+		--session \
+		--print-reply \
+		--dest=net.launchpad.ulauncher \
+		/net/launchpad/ulauncher \
+		net.launchpad.ulauncher.toggle_window
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash wofi 2>/dev/null; then
 	wofi --show drun --allow-images --insensitive --prompt "Applications"
-elif hash sgtk-grid 2>/dev/null; then
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash sgtk-grid 2>/dev/null; then
 	sgtk-grid -t 60 -b 28 -o 0.8 -f
-elif hash kitty 2>/dev/null; then
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash kitty 2>/dev/null; then
 	TERMINAL_COMMAND="kitty" kitty --class app-menu -e ~/.config/sway/sway-launcher-desktop.sh
-elif hash termite 2>/dev/null; then
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash termite 2>/dev/null; then
 	TERMINAL_COMMAND="termite -e" termite --name app-menu -e ~/.config/sway/sway-launcher-desktop.sh
-elif hash mako 2>/dev/null; then
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash mako 2>/dev/null; then
 	notify-send 'No application to display menu' 'Please, install sgtk-menu, kitty or termite'
-elif hash swaynag 2>/dev/null; then
+	RESULT=$?
+fi
+if [ $RESULT -ne 0 ] && hash swaynag 2>/dev/null; then
 	swaynag -m 'No application to display menu. Please, install sgtk-menu, kitty or termite'
 fi
